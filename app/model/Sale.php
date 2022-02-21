33d633211ad1bada101dccbc07b199a1a200d12a
grgic.matej <grgic.matej@gmail.com>
2022-02-21 15:55:20 +0100

    membership purchase bugfix

--- app/model/Sale.php
+++ app/model/Sale.php
@@ -8,23 +8,29 @@ class Sale
         $db = Db::getInstance();
         $stmt = $db->prepare('INSERT INTO Sales 
                             (
-                            Sales_Gym_Id,
-                            Sales_Item_Name,
-                            Sales_Price,
-                            Sales_Date
+                             Sales_Gym_Id,
+                             Sales_Item_Id,
+                             Sales_Item_Name,
+                             Sales_Price,
+                             Sales_Date,
+                             Sales_Staff_Id
                             )
                             VALUES
                             (
-                            :salesGymId,
-                            :salesItemName,
-                            :salesPrice,
-                            :salesDate
+                             :salesGymId,
+                             :salesItemId,
+                             :salesItemName,
+                             :salesPrice,
+                             :salesDate,
+                             :salesStaffId
                             )
                             ');
         $stmt->bindValue('salesGymId', $_SESSION['Gym_Id']);
+        $stmt->bindValue('salesItemId', $membershipData->Memberships_Id);
         $stmt->bindValue('salesItemName', $membershipData->Memberships_Name);
         $stmt->bindValue('salesPrice', $membershipData->Memberships_Price);
         $stmt->bindValue('salesDate', date('Y-m-d'));
+        $stmt->bindValue('salesStaffId', Session::getInstance()->getUser()->Staff_Id);
         $stmt->execute();
     }
 }
