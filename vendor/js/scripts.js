c977075d0a5c62ba2d71fe19d5a9ce54f99f8fe3
grgic.matej <grgic.matej@gmail.com>
2022-02-22 16:23:16 +0100

    adding advanced settings option in profile user (testing only)

--- vendor/js/scripts.js
+++ vendor/js/scripts.js
@@ -13,3 +13,10 @@ $.ajax({
 });
     ajax primjer kraj
 */
+
+
+$('#testgumb').on('click', function () {
+    document.getElementById("test12").style.display = "block";
+});
+
+//gumb radi za pojavljivanje opcija, treba to finesirati, neki fade in napravit
