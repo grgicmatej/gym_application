be1387448dc6f87f4779a6ff18d43570680ab52f
grgic.matej <grgic.matej@gmail.com>
2022-04-07 09:08:47 +0200

    .

--- vendor/js/Functionalities/statistics.js
+++ vendor/js/Functionalities/statistics.js
@@ -34,17 +34,8 @@ $('.statistics').on('click', function () {
     {
         var areaChartCanvas = $('#areaChart2').get(0).getContext('2d')
 
-
-        // dobije string
-        // sa parseom prebaci u obj
-
-
         let result = JSON.parse(responseCurrentYear).map(({ userData }) => userData)
-
-
         let result1 = JSON.parse(responsePreviousYear).map(({ userData }) => userData)
-
-
 
         var areaChartData = {
             labels  : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
@@ -53,7 +44,7 @@ $('.statistics').on('click', function () {
                     label               :  new Date().getFullYear(),
                     backgroundColor     : 'rgba(60,141,188,0.9)',
                     borderColor         : 'rgba(60,141,188,0.8)',
-                    pointRadius          : false,
+                    pointRadius          : true,
                     pointColor          : '#3b8bba',
                     pointStrokeColor    : 'rgba(60,141,188,1)',
                     pointHighlightFill  : '#fff',
@@ -64,7 +55,7 @@ $('.statistics').on('click', function () {
                     label               : new Date().getFullYear()-1,
                     backgroundColor     : 'rgba(210, 214, 222, 1)',
                     borderColor         : 'rgba(210, 214, 222, 1)',
-                    pointRadius         : false,
+                    pointRadius         : true,
                     pointColor          : 'rgba(210, 214, 222, 1)',
                     pointStrokeColor    : '#c1c7d1',
                     pointHighlightFill  : '#fff',
